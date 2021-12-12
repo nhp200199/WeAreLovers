9df8aceec919dfab18466efb854543bf18f0b8bc
Phuc Nguyen <nhp200199@gmail.com>
2021-12-12 21:53:55 +0700

    integrate interstitial ad

--- app/src/main/java/com/example/lovereminder/DiaryFragment.java
+++ app/src/main/java/com/example/lovereminder/DiaryFragment.java
@@ -31,6 +31,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lovereminder.databinding.FragmentDiaryBinding;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.jetbrains.annotations.NotNull;
@@ -62,6 +68,11 @@ public class DiaryFragment extends Fragment {
     private FragmentDiaryBinding binding;
     private Disposable mGetAllDiaryDisposable;
 
+    private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712";
+    private InterstitialAd interstitialAd;
+
+    private int touchesToOpenAd = 5;
+
     public DiaryFragment() {
         // Required empty public constructor
     }
@@ -87,10 +98,73 @@ public class DiaryFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_diary, container, false);
         binding = FragmentDiaryBinding.bind(v);
+        loadAd();
+
         connectViews(binding);
 
         loadDiaries();
+
         return v;
+    }
+
+    private void loadAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        InterstitialAd.load(
+                getActivity(),
+                AD_UNIT_ID,
+                adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        DiaryFragment.this.interstitialAd = interstitialAd;
+                        Log.i(TAG, "onAdLoaded");
+                        Toast.makeText(getActivity(), "onAdLoaded()", Toast.LENGTH_SHORT).show();
+                        interstitialAd.setFullScreenContentCallback(
+                                new FullScreenContentCallback() {
+                                    @Override
+                                    public void onAdDismissedFullScreenContent() {
+                                        // Called when fullscreen content is dismissed.
+                                        // Make sure to set your reference to null so you don't
+                                        // show it a second time.
+                                        DiaryFragment.this.interstitialAd = null;
+                                        Log.d("TAG", "The ad was dismissed.");
+                                    }
+
+                                    @Override
+                                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                        // Called when fullscreen content failed to show.
+                                        // Make sure to set your reference to null so you don't
+                                        // show it a second time.
+                                        DiaryFragment.this.interstitialAd = null;
+                                        Log.d("TAG", "The ad failed to show.");
+                                    }
+
+                                    @Override
+                                    public void onAdShowedFullScreenContent() {
+                                        // Called when fullscreen content is shown.
+                                        DiaryFragment.this.interstitialAd = null;
+                                        Log.d("TAG", "The ad was shown.");
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i(TAG, loadAdError.getMessage());
+                        interstitialAd = null;
+
+                        String error =
+                                String.format(
+                                        "domain: %s, code: %d, message: %s",
+                                        loadAdError.getDomain(), loadAdError.getCode(), loadAdError.getMessage());
+                        Toast.makeText(
+                                getActivity(), "onAdFailedToLoad() with error: " + error, Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
     }
 
     private void connectViews(FragmentDiaryBinding binding) {
@@ -163,8 +237,23 @@ public class DiaryFragment extends Fragment {
         fabAddDiary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(requireActivity(), CreateDiaryActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(requireActivity(), CreateDiaryActivity.class);
+//                startActivity(intent);
+                touchesToOpenAd --;
+                if (touchesToOpenAd == 1) {
+                    loadAd();
+                }
+                else if (touchesToOpenAd == 0) {
+                    if (interstitialAd != null) {
+                        interstitialAd.show(getActivity());
+                        touchesToOpenAd = (int) (Math.random() * 4 + 2); //range from 2 to 5
+                        Toast.makeText(getActivity(), "New touches set: " + touchesToOpenAd, Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        touchesToOpenAd = 1;
+                        loadAd();
+                    }
+                }
             }
         });
     }
