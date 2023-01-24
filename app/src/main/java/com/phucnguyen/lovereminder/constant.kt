3ab9f7035aea8663f1f863ec5e0cf82ad57038a7
Phuc Nguyen <nhp200199@gmail.com>
2023-01-24 10:45:20 +0700

    refactor welcome screen

--- app/src/main/java/com/phucnguyen/lovereminder/constant.kt
+++ app/src/main/java/com/phucnguyen/lovereminder/constant.kt
@@ -3,6 +3,7 @@ package com.phucnguyen.lovereminder
 // shared preference
 const val SHARE_PREF_USER_INFO = "userInfor"
 const val PREF_COUPLE_DATE = "date"
+const val PREF_YOUR_NAME = "yourName"
 
 const val SHARE_PREF_BACKGROUND = "background"
 const val PREF_PICTURE = "picture"
--- app/src/main/java/com/phucnguyen/lovereminder/ui/activity/WelcomeScreen.kt
+++ app/src/main/java/com/phucnguyen/lovereminder/ui/activity/WelcomeScreen.kt
@@ -7,6 +7,9 @@ import com.phucnguyen.lovereminder.R
 import android.widget.TextView
 import android.content.Intent
 import android.view.animation.AnimationUtils
+import com.phucnguyen.lovereminder.PREF_YOUR_NAME
+import com.phucnguyen.lovereminder.SHARE_PREF_USER_INFO
+import com.phucnguyen.lovereminder.databinding.ActivityWelcomeScreenBinding
 import com.phucnguyen.lovereminder.ui.activity.IniActivity
 import com.phucnguyen.lovereminder.ui.activity.MainActivity
 
@@ -14,16 +17,17 @@ class WelcomeScreen : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome_screen)
-        sharedPreferences = getSharedPreferences("userInfor", MODE_PRIVATE)
-        val tvWelcome = findViewById<TextView>(R.id.tv_welcome)
+        val binding = ActivityWelcomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        sharedPreferences = getSharedPreferences(SHARE_PREF_USER_INFO, MODE_PRIVATE)
+        val tvWelcome = binding.tvWelcome
         val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.fade)
         tvWelcome.animation = animation
         val timer: Thread = object : Thread() {
             override fun run() {
                 try {
                     sleep(3000)
-                    if (sharedPreferences.getString("yourName", "") === "") {
+                    if (sharedPreferences.getString(PREF_YOUR_NAME, "")!!.isEmpty()) {
                         val intent = Intent(applicationContext, IniActivity::class.java)
                         startActivity(intent)
                     } else {
