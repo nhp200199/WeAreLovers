80cf57a0adaba7e180b9c5ba51418f519385e171
hungphuc.ng <hungphuc.ng>
2023-03-23 10:48:09 +0700

    remove un-used methods and variables

--- app/src/main/java/com/phucnguyen/lovereminder/ui/adapter/ImageAdapter.kt
+++ app/src/main/java/com/phucnguyen/lovereminder/ui/adapter/ImageAdapter.kt
@@ -23,7 +23,6 @@ class ImageAdapter(var context: Context) : RecyclerView.Adapter<ImageAdapter.Vie
             notifyDataSetChanged()
         }
     var listener: Listener? = null
-    val imagesToDelete: SparseBooleanArray = SparseBooleanArray()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemPictureBinding.inflate(LayoutInflater.from(context))
         return ViewHolder(binding)
@@ -48,44 +47,6 @@ class ImageAdapter(var context: Context) : RecyclerView.Adapter<ImageAdapter.Vie
 
     override fun getItemCount(): Int {
         return images?.size ?: 0
-    }
-
-//    override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
-//        val imageView = ImageView(context)
-//        if (imagesToDelete[position]) {
-//            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-//            imageView.layoutParams = ViewGroup.LayoutParams(330, 350)
-//            imageView.cropToPadding = true
-//            Glide.with(context)
-//                .load(images[position].uri)
-//                .thumbnail(0.5f)
-//                .into(imageView)
-//            imageView.setBackgroundResource(R.drawable.view_border)
-//        } else {
-//            Glide.with(context)
-//                .load(images[position].uri)
-//                .into(imageView)
-//            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-//            imageView.layoutParams = ViewGroup.LayoutParams(330, 350)
-//        }
-//        return imageView
-//    }
-
-    fun toggleImagePositionToDelete(position: Int): Boolean {
-        val result: Boolean
-        if (imagesToDelete[position]) {
-            result = false
-            imagesToDelete.delete(position)
-        } else {
-            result = true
-            imagesToDelete.put(position, true)
-        }
-        notifyDataSetChanged()
-        return result
-    }
-
-    fun resetTracker() {
-        imagesToDelete.clear()
     }
 
     class ViewHolder(private val binding: ItemPictureBinding) : RecyclerView.ViewHolder(binding.root) {
