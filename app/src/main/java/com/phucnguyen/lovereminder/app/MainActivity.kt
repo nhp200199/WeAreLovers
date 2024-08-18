c0108c7f4ad89712e30a01b6fb64bd71e05aab97
nhp2000199 <nhp200199@gmail.com>
2024-08-18 15:27:41 +0700

    integrate test admob

--- app/src/main/java/com/phucnguyen/lovereminder/app/MainActivity.kt
+++ app/src/main/java/com/phucnguyen/lovereminder/app/MainActivity.kt
@@ -4,10 +4,13 @@ import com.phucnguyen.lovereminder.feature.couple.viewer.presentation.MainFragme
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.PowerManager
 import android.provider.Settings
 import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowMetrics
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
@@ -19,6 +22,14 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.bumptech.glide.Glide
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
+import com.phucnguyen.lovereminder.R
 import com.phucnguyen.lovereminder.core.base.presentation.BaseActivity
 import com.phucnguyen.lovereminder.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,6 +41,57 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     private val viewModel: MainActivityViewModel by viewModels()
     private var closeAppFlag = 0 // used to check exit
     private lateinit var timer: CountDownTimer
+    // Get the ad size with screen width.
+    private val adSize: AdSize
+        get() {
+            val displayMetrics = resources.displayMetrics
+            val adWidthPixels =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    val windowMetrics: WindowMetrics = this.windowManager.currentWindowMetrics
+                    windowMetrics.bounds.width()
+                } else {
+                    displayMetrics.widthPixels
+                }
+            val density = displayMetrics.density
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        Log.d("Tag", "Created")
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        setSupportActionBar(binding.toolbar.tb)
+//        supportActionBar!!.setDisplayShowTitleEnabled(false)
+////        retrieveUserInfor()
+//        setUpViewPager()
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isIgnoringBatteryOptimizations(this)) {
+//            openIgnoreBatteryOptimizationSettings()
+//        }
+
+        initializeMobileAdsSdk()
+        setupAdView()
+    }
+
+    private fun initializeMobileAdsSdk() {
+        // Initialize the Mobile Ads SDK with an AdMob App ID.
+        MobileAds.initialize(this) {}
+
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder().setTestDeviceIds(
+                listOf(
+                    "AC2FDCECBCC0ADA1D187ED08618252FD",
+                    "41C49D16BFE9FC2EF7D54549D51BCB47" //pixel 5
+                )
+            ).build()
+        )
+    }
+
     override fun getClassTag(): String {
         return MainActivity::class.java.simpleName
     }
@@ -67,108 +129,64 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
         })
     }
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var adView: AdView
+    private var initialLayoutComplete = false
 
-//    private lateinit var adView: AdView
-//    private var initialLayoutComplete = false
-//    // Determine the screen width (less decorations) to use for the ad width.
-//    // If the ad hasn't been laid out, default to the full screen width.
-//    private val myAdSize: AdSize
-//        get() {
-//            val display = windowManager.defaultDisplay
-//            val outMetrics = DisplayMetrics()
-//            display.getMetrics(outMetrics)
-//
-//            val density = outMetrics.density
-//
-//            var adWidthPixels = binding.adViewContainer.width.toFloat()
-//            if (adWidthPixels == 0f) {
-//                adWidthPixels = outMetrics.widthPixels.toFloat()
-//            }
-//
-//            val adWidth = (adWidthPixels / density).toInt()
-//            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
-//        }
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        Log.d("Tag", "Created")
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//        setSupportActionBar(binding.toolbar.tb)
-//        supportActionBar!!.setDisplayShowTitleEnabled(false)
-////        retrieveUserInfor()
-//        setUpViewPager()
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isIgnoringBatteryOptimizations(this)) {
-//            openIgnoreBatteryOptimizationSettings()
-//        }
-////        setupAdView()
-//    }
+    private fun setupAdView() {
+        adView = AdView(this)
+        adView.adListener = object : AdListener() {
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
 
-//    private fun setupAdView() {
-//        // Initialize the Mobile Ads SDK with an AdMob App ID.
-//        MobileAds.initialize(this) {}
-//
-//        // Set your test devices. Check your logcat output for the hashed device ID to
-//        // get test ads on a physical device. e.g.
-//        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-//        // to get test ads on this device."
-//        MobileAds.setRequestConfiguration(
-//            RequestConfiguration.Builder().setTestDeviceIds(listOf("AC2FDCECBCC0ADA1D187ED08618252FD")).build()
-//        )
-//
-//        adView = AdView(this)
-//        adView.adListener = object : AdListener() {
-//            override fun onAdClicked() {
-//                // Code to be executed when the user clicks on an ad.
-//            }
-//
-//            override fun onAdClosed() {
-//                // Code to be executed when the user is about to return
-//                // to the app after tapping on an ad.
-//            }
-//
-//            override fun onAdFailedToLoad(adError: LoadAdError) {
-//                // Code to be executed when an ad request fails.
-//            }
-//
-//            override fun onAdImpression() {
-//                // Code to be executed when an impression is recorded
-//                // for an ad.
-//            }
-//
-//            override fun onAdLoaded() {
-//                binding.adViewContainer.visibility = View.VISIBLE
-//            }
-//
-//            override fun onAdOpened() {
-//                // Code to be executed when an ad opens an overlay that
-//                // covers the screen.
-//            }
-//        }
-//
-//        binding.adViewContainer.addView(adView)
-//        // Since we're loading the banner based on the adContainerView size, we need to wait until this
-//        // view is laid out before we can get the width.
-//        binding.adViewContainer.viewTreeObserver.addOnGlobalLayoutListener {
-//            if (!initialLayoutComplete) {
-//                initialLayoutComplete = true
-//                loadBanner()
-//            }
-//        }
-//    }
-//
-//    private fun loadBanner() {
-//        adView.adUnitId = getString(R.string.banner_ad_unit_id)
-//
-//        adView.adSize = myAdSize
-//
-//        // Create an ad request.
-//        val adRequest = AdRequest.Builder().build()
-//
-//        // Start loading the ad in the background.
-//        adView.loadAd(adRequest)
-//    }
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            override fun onAdImpression() {
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            override fun onAdLoaded() {
+                binding.adViewContainer.visibility = View.VISIBLE
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+        }
+
+        binding.adViewContainer.removeAllViews()
+        binding.adViewContainer.addView(adView)
+        // Since we're loading the banner based on the adContainerView size, we need to wait until this
+        // view is laid out before we can get the width.
+        binding.adViewContainer.viewTreeObserver.addOnGlobalLayoutListener {
+            if (!initialLayoutComplete) {
+                initialLayoutComplete = true
+                loadBanner()
+            }
+        }
+    }
+
+    private fun loadBanner() {
+        adView.adUnitId = getString(R.string.banner_ad_unit_id)
+
+        adView.setAdSize(adSize)
+
+        // Create an ad request.
+        val adRequest = AdRequest.Builder().build()
+
+        // Start loading the ad in the background.
+        adView.loadAd(adRequest)
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     private fun isIgnoringBatteryOptimizations(context: Context): Boolean {
@@ -235,17 +253,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     public override fun onResume() {
         super.onResume()
 //        if (intent.hasExtra("position")) swipeViewPager(intent.getIntExtra("position", 0))
-//        adView.resume()
+        adView.resume()
     }
 
     override fun onPause() {
         super.onPause()
-//        adView.pause()
+        adView.pause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-//        adView.destroy()
+        adView.destroy()
     }
 
     private class SectionsPagerAdapter(fm: FragmentManager?) : FragmentPagerAdapter(
